760e1a940cd16d2205635a1a054f889a246dfad6
Siddharth Muralee <siddharth.muralee@gmail.com>
2022-04-28 18:15:42 -0400

    small change

--- vmscripts/build-fs.sh
+++ vmscripts/build-fs.sh
@@ -5,6 +5,6 @@ mkdir -p $HOME/playground/staging/initramfs/fs
 cd $HOME/playground/staging/initramfs/fs
 mkdir -pv bin sbin etc proc sys usr/bin usr/sbin
 cp -av $HOME/playground/sources/busybox-1.32.1/_install/* .
-cp $HOME/playground/scripts/init .
+cp $HOME/playground/linux-playground/scripts/init .
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > $HOME/playground/staging/initramfs-busybox-x86.cpio.gz
 cd $HOME/playground/staging
