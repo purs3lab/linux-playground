d2f9a618fe29a9e1468aa7fcec78c503ffb98c3e
Aravind Machiry <amachiry@purdue.edu>
2025-01-29 16:29:17 -0500

    Adding all

--- demos/memory_testing/sample.c
+++ demos/memory_testing/sample.c
@@ -4,13 +4,13 @@ int main(int argc, char **argv) {
   printf("Trying to malloc 2 pages..press enter..\n");
   getchar();
   p = malloc(2*4096);
-  printf("Trying to access first page..press enter..\n");
+  printf("Got address: %p, Trying to access first page..press enter..\n", p);
   getchar();
   p[0] = 'a';
   printf("Trying to access first page again..press enter..\n");
   getchar();
   p[128] = 'a';
-  printf("Trying to access second page..press enter..\n");
+  printf("Trying to access second page (address: %p)..press enter..\n", p + 5000);
   getchar();
   p[5000] = 'a';
   printf("Trying to access invalid memory..press enter..\n");
